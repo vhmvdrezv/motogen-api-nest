bf3959932c1e9678a7dcecf10200b76968e56ba3
vhmvdrezv <ahmdrzashn@gmail.com>
2025-08-05 05:17:33 +0330

    add swagger to update car

--- src/user-cars/dto/update-car.dto.ts
+++ src/user-cars/dto/update-car.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from "@nestjs/mapped-types";
 import { CreateCarDto } from "./create-car.dto";
+import { PartialType } from "@nestjs/swagger";
 
 export class UpdateCarDto extends PartialType(CreateCarDto) { }
