97347ae740a896ed8dc47f6fcbbf3b80b5bc117d
vhmvdrezv <ahmdrzashn@gmail.com>
2025-08-14 23:44:21 +0330

    delete ai controller

--- src/ai/ai.module.ts
+++ src/ai/ai.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AiService } from './ai.service';
-import { AiController } from './ai.controller';
 
 @Module({
   providers: [AiService],
-  controllers: [AiController],
   exports: [AiService],
 })
 export class AiModule {}
